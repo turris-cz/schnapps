b9a54cf6a35ab5bb60705004983dd681816d7aef
Karel Kočí <karel.koci@nic.cz>
2020-01-07 12:28:57 +0100

    Revert "Add support for certbackup"
    This reverts commit 7ad0ba8301f61a465a4668915c1198149278bd5f.
    
    Certificate backup restore is handled by hook run by schnapps so this
    code duplicates that functionality. It should not be there.
    
    It is also wrong as some of the files are hidden (starting with dot)
    and this copy omits them. At the same time it prints error if there is
    no file in store (this is "fixed" by hiding errors but nooo).

--- schnapps.sh
+++ schnapps.sh
@@ -389,9 +389,6 @@ rollback() {
                 [ \! -x "$i" ] || "$i" "$TMP_MNT_DIR"/@
             done
         fi
-        if [ -d "$TMP_MNT_DIR"/certbackup ]; then
-            cp -ra "$TMP_MNT_DIR"/certbackup/* "$TMP_MNT_DIR"/@ 2> /dev/null >&2
-        fi
     else
         rm -f "$TMP_MNT_DIR"/$NUMBER.info
         mv "$TMP_MNT_DIR"/@$NUMBER "$TMP_MNT_DIR"/@
