e93271a871712d08e58573c516f1d78f98ab8d35
Michal Hrusecky <michal.hrusecky@turris.com>
2021-07-12 08:43:27 +0200

    Allow to keep remote snapshots forever
    Sometimes you have a lot space on your server and want to delete backups
    manually.

--- schnapps.sh
+++ schnapps.sh
@@ -29,22 +29,24 @@ KEEP_MAX=""
 REMOTE_URL=""
 REMOTE_MOUNTED=""
 REMOTE_PATH=""
+REMOTE_KEEP="0"
 GPG_PASS=""
 ROOT_DEV="$(btrfs fi show / | sed -n 's|.*\(/dev/[^[:blank:]]*\)$|\1|p' | head -n 1)"
 VERSION="@VERSION@"
 
 # Read configuration
 if [ -n "`which uci 2> /dev/null`" ]; then
-    KEEP_MAX_SINGLE="`  uci get schnapps.keep.max_single   2> /dev/null`"
-    KEEP_MAX_TIME="`    uci get schnapps.keep.max_time     2> /dev/null`"
-    KEEP_MAX_UPDATER="` uci get schnapps.keep.max_updater  2> /dev/null`"
-    KEEP_MAX_ROLLBACK="`uci get schnapps.keep.max_rollback 2> /dev/null`"
-    REMOTE_URL="`       uci get schnapps.remote.url        2> /dev/null`"
-    REMOTE_PATH="`      uci get schnapps.remote.path       2> /dev/null`"
-    REMOTE_USER="`      uci get schnapps.remote.user       2> /dev/null`"
-    REMOTE_PASS="`      uci get schnapps.remote.password   2> /dev/null`"
-    SYNC_TYPES="`       uci get schnapps.remote.sync_types 2> /dev/null`"
-    GPG_PASS="`         uci get schnapps.encrypt.pass      2> /dev/null`"
+    KEEP_MAX_SINGLE="`  uci get schnapps.keep.max_single      2> /dev/null`"
+    KEEP_MAX_TIME="`    uci get schnapps.keep.max_time        2> /dev/null`"
+    KEEP_MAX_UPDATER="` uci get schnapps.keep.max_updater     2> /dev/null`"
+    KEEP_MAX_ROLLBACK="`uci get schnapps.keep.max_rollback    2> /dev/null`"
+    REMOTE_URL="`       uci get schnapps.remote.url           2> /dev/null`"
+    REMOTE_PATH="`      uci get schnapps.remote.path          2> /dev/null`"
+    REMOTE_USER="`      uci get schnapps.remote.user          2> /dev/null`"
+    REMOTE_PASS="`      uci get schnapps.remote.password      2> /dev/null`"
+    REMOTE_KEEP="`      uci get schnapps.remote.keep_foreever 2> /dev/null`"
+    SYNC_TYPES="`       uci get schnapps.remote.sync_types    2> /dev/null`"
+    GPG_PASS="`         uci get schnapps.encrypt.pass         2> /dev/null`"
 fi
 
 [ \! -f /etc/schnapps/config ] || . /etc/schnapps/config
@@ -736,6 +738,10 @@ upload() {
     export_sn "$NUM" "$TMP_RMT_MNT_DIR""$REMOTE_PATH" | sed "s|^\\([^.].*exported.*\\)$TMP_RMT_MNT_DIR|\\1$REMOTE_URL|"
 }
 
+info2num() {
+    echo "$1" | sed -n 's|.*[/-]\([0-9]*\).info$|\1|'
+}
+
 sync_snps() {
     remote_mount
     if [ "x$1" = "x-t" ]; then
@@ -743,20 +749,25 @@ sync_snps() {
         [ "$2" \!= all ] || SYNC_TYPES="$DEFAULT_SYNC_TYPES"
     fi
     get_board
-    NUMS="$(ls -1 "$TMP_MNT_DIR"/*.info | sed 's|.*/\([0-9]*\).info$|\1|')"
-    RM_NUMS="$(ls -1 "$TMP_RMT_MNT_DIR"/"$REMOTE_PATH"/*-medkit-$HOSTNAME-*.info 2> /dev/null | sed 's|.*/.*-\([0-9]*\).info$|\1|')"
-    for i in $NUMS; do
-        . "$TMP_MNT_DIR"/"$i".info
+    for info in "$TMP_MNT_DIR"/*.info; do
+        [ -f "$info" ] || continue
+        . "$info"
+        local num="$(info2num "$info")"
         if expr "$SYNC_TYPES" : ".*$TYPE.*" > /dev/null; then
-            [ -f "$TMP_RMT_MNT_DIR"/"$REMOTE_PATH"/"$BOARD-medkit-$HOSTNAME-$i.info" ] || upload "$i"
+            [ -f "$TMP_RMT_MNT_DIR/$REMOTE_PATH/$BOARD-medkit-$HOSTNAME-$num.info" ] \
+                || upload "$i"
         fi
-        RM_NUMS="$(echo "$RM_NUMS" | grep -v "^$i\$")"
     done
-    for i in $RM_NUMS; do
-        rm -f "$TMP_RMT_MNT_DIR"/"$REMOTE_PATH"/"$BOARD-medkit-$HOSTNAME-$i.info" \
-              "$TMP_RMT_MNT_DIR"/"$REMOTE_PATH"/"$BOARD-medkit-$HOSTNAME-$i.tar.gz" \
-              "$TMP_RMT_MNT_DIR"/"$REMOTE_PATH"/"$BOARD-medkit-$HOSTNAME-$i.tar.gz.pgp"
-    done
+    if [ "$REMOTE_KEEP" != 1 ]; then
+        for info in "$TMP_RMT_MNT_DIR/$REMOTE_PATH/$BOARD-medkit-$HOSTNAME-"*.info; do
+            [ -f "$info" ] || continue
+            local num="$(info2num "$info")"
+            [ -f "$TMP_MNT_DIR/$num.info" ] || continue
+            rm -f "$TMP_RMT_MNT_DIR/$REMOTE_PATH/$BOARD-medkit-$HOSTNAME-$num.info" \
+                  "$TMP_RMT_MNT_DIR/$REMOTE_PATH/$BOARD-medkit-$HOSTNAME-$num.tar.gz" \
+                  "$TMP_RMT_MNT_DIR/$REMOTE_PATH/$BOARD-medkit-$HOSTNAME-$num.tar.gz.pgp"
+        done
+    fi
 }
 
 remote_list() {
