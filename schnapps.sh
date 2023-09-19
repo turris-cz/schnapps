d619fac888e4d2f369b9c82d776d8ee3495f50f5
Richard Muzik <richard.muzik@nic.cz>
2023-09-19 09:04:11 +0200

    Fix ssh url regex
    The previous regex was matching strings that shouldn't have been
    matched, resulting in colons being placed in the wrong spots.
    Url "ssh://user@host:path/path" transformed into "user@host:path:/path"

--- schnapps.sh
+++ schnapps.sh
@@ -716,7 +716,7 @@ remote_mount() {
             webdav_mount
             ;;
         ssh://*)
-            FINAL_REMOTE_URL="$(echo "$REMOTE_URL" | sed -e 's|ssh://||' | sed -e 's|\:*\(\/.*\)|:\1|')"
+            FINAL_REMOTE_URL="$(echo "$REMOTE_URL" | sed -e 's|ssh://||' | sed -e 's|^\([^:/]*\)/|\1:/|')"
             expr "$FINAL_REMOTE_URL" : '.*:' > /dev/null || FINAL_REMOTE_URL="$FINAL_REMOTE_URL:"
             [ -n "`which sshfs`" ] || die "sshfs is not available"
             sshfs "$FINAL_REMOTE_URL" "$TMP_RMT_MNT_DIR" || die "Can't access remote filesystem"
